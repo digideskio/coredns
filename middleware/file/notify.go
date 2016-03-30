0f84eb9a0460017b56e48fd14e76b7e33d4793ec
Miek Gieben <miek@miek.nl>
2016-03-30 22:49:14 +0100

    Add notifies to master servers
    On startup (only!) send notifies to the NS list of a zone. Try sending
    three times before giving up.

--- middleware/file/notify.go
+++ middleware/file/notify.go
@@ -1,8 +1,39 @@
 package file
 
-// Notify sends notifies to the configured remotes. It will try up to three times
-// before giving up on a specific remote.
-func Notify(remotes []string) error {
-	return nil
+import (
+	"fmt"
 
+	"github.com/miekg/coredns/middleware"
+	"github.com/miekg/dns"
+)
+
+// Notify will ...
+func (z *Zone) Notify() error {
+	return nil
+}
+
+// Notify sends notifies to the configured remotes. It will try up to three times
+// before giving up on a specific remote. We will sequentially loop through the remotes
+// until they all have replied (or have 3 failed attempts).
+func Notify(zone string, remotes []string) error {
+	m := new(dns.Msg)
+	m.SetNotify(zone)
+	c := new(dns.Client)
+
+	// TODO(miek): error handling?
+	for _, remote := range remotes {
+		notifyRemote(c, m, remote)
+	}
+	return nil
+}
+
+func notifyRemote(c *dns.Client, m *dns.Msg, s string) error {
+	for i := 0; i < 3; i++ {
+		ret, err := middleware.Exchange(c, m, s)
+		if err == nil && ret.Rcode == dns.RcodeSuccess || ret.Rcode == dns.RcodeNotImplemented {
+			return nil
+		}
+		// timeout? mean don't want it. should stop sending as well
+	}
+	return fmt.Errorf("failed to send notify for zone '%s' to '%s'", m.Question[0].Name, s)
 }
